461e1002a84d665b6a14cfa892b0bbad7b579c33
Jay <jmartinsmith@outlook.com>
2021-12-09 22:20:21 +0000

    removed unused interface

--- src/components/StatusBar/presentation/interface.ts
+++ src/components/StatusBar/presentation/interface.ts
@@ -1,7 +1,3 @@
-interface onClickPayload {
-    currentState: "connect" | "disconnect" | "transitioning"
-}
-
 export interface StatusBarBadgeProps {
     icon: () => any,
     color: "default" | "primary" | "secondary" | "error" | "info" | "success" | "warning" | undefined,
