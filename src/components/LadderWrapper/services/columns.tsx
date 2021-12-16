4dd2da4619ffd52527c338944ccd9a903ea09449
Jay <jmartinsmith@outlook.com>
2021-12-16 11:55:21 +0000

    header alignment: https://github.com/dweng0/glowing-palm-tree/issues/6

--- src/components/LadderWrapper/services/columns.tsx
+++ src/components/LadderWrapper/services/columns.tsx
@@ -1,15 +1,6 @@
 import { FeedType } from "../../../interface";
 import { GridRenderCellParams, GridColDef } from "@mui/x-data-grid";
 
- /**
- * Column to be provided to the ladders, reversed where required
- */
-const columns = [
-    
-    { field: "size", headername: "Size", flex: 1 },
-    { field: "total", headername: "Total",  flex: 1 }
-];
-
 const getPriceField = (color: "red" | "green") => { 
     return { 
         field: "price", 
@@ -17,17 +8,40 @@ const getPriceField = (color: "red" | "green") => {
         flex: 1, 
         renderCell: (params: GridRenderCellParams<number>) => (
         <span style={{color: (color === "red") ? "tomato" : "#04AA6D"}}>
-            {params.value}     
+            {(params.value).toLocaleString("en", {style: "currency", currency: "USD", minimumFractionDigits: 2})}
         </span>
     )}
 }
+
+
+ /**
+ * Column to be provided to the ladders, reversed where required
+ */
+const columns = [
+    { field: "size", headername: "Size", flex: 1, 
+    renderCell:  (params: GridRenderCellParams<number>) => (
+        <span>
+            {(params.value).toLocaleString()}
+        </span>
+    )},
+    { field: "total", headername: "Total",  flex: 1,
+    renderCell:  (params: GridRenderCellParams<number>) => (
+        <span>
+            {(params.value).toLocaleString()}
+        </span>
+    )}
+];
+
+const rightAlignedProperties = {align: "right" as "right", headerAlign: "right" as "right"};
+
+const rightAligned = () => columns.map(item => ({...item, ...rightAlignedProperties}));
 
 export const getColumns = (feedType: FeedType): Array<GridColDef> => { 
     switch (feedType) { 
         case "asks": 
             return [...[getPriceField("red")], ...columns.reverse()];
         case "bids":
-            return [...columns, ...[getPriceField("green")]];
+            return [...rightAligned(), ...[{...getPriceField("green"), ...rightAlignedProperties}]];
         default:
             throw new Error(`Unknown feed type ${feedType}`);
     }
