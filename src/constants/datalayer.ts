626e03c99aeaef161e1f60f5a79390d27f9fda13
Jay <jmartinsmith@outlook.com>
2021-12-20 08:17:05 +0000

    increased speed for buffer

--- src/constants/datalayer.ts
+++ src/constants/datalayer.ts
@@ -9,7 +9,7 @@ export const MAX_CONNECTION_ATTEMPTS = 10;
 export const RETRY_DEBOUNCE = 2500 
 
 // number of mseconds to wait before flushing the buffer
-export const UPDATE_SPEED = 800;
+export const UPDATE_SPEED = 1300;
 
 
 // The subscription message
